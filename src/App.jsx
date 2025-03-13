30f9fc3694e1f4e9656535a2e87cb9ba3915b71c
Danish Haroon <danishmohammedharoon@gmail.com>
2025-03-13 04:59:03 -0230

    sets up files and wireframe base

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.scss'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
